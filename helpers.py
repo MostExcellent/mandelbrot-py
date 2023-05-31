ad7f14ff3175a1fc47dff342f12324d72891cb3d
MostExcellent <dwglover3@gmail.com>
2023-05-31 14:25:11 -0700

    Update and fomatting

--- helpers.py
+++ helpers.py
@@ -1,23 +1,31 @@
-from PIL import Image 
+from PIL import Image
+
 
 def linterp(myA, myB, myF):
-	# interpolates two values myA and myB with a fraction myF
-	return float((1-myF) * myA + myF * myB)
+    """
+    Interpolates two values myA and myB with a fraction myF.
+    """
+    return (1 - myF) * myA + myF * myB
+
 
 def mapRange(v, min1, max1, min2, max2):
-	# value is the value to map
-	# min1 is the minimum value of the range n is in and max1 is its maximum value
-	# min2 is the minimum value of the range n is to be mapped to and max1 is its maximum value
-	return float(v-min1)/float(max1-min1)*float(max2-min2)+min2
+    """
+    Maps a value v from the range [min1, max1] to the range [min2, max2].
+    """
+    return (v - min1) / (max1 - min1) * (max2 - min2) + min2
 
-def imgFromTuples(m,s,d):
-	# s is a (width,height) tuple
-	# d is a list of RGB tuples
-	# m is the desired PIL mode
-	img = Image.new(m,s)
-	img.putdata(d)
-	return img
+
+def imgFromTuples(mode, size, data):
+    """
+    Creates an image from a list of RGB tuples.
+    """
+    img = Image.new(mode, size)
+    img.putdata(data)
+    return img
+
 
 def colorMap(mnic):
-	# turns a mapped normalized iteration count for a pixel of a mandelbrot plot into an RGB tuple
-	return (255-mnic,abs(128-mnic),mnic)
+    """
+    Converts a mapped normalized iteration count for a pixel of a Mandelbrot plot into an RGB tuple.
+    """
+    return (255 - mnic, abs(128 - mnic), mnic)
--- mandelbrot.py
+++ mandelbrot.py
@@ -5,48 +5,61 @@ from datetime import datetime
 from argparse import ArgumentParser
 
 def mandelbrot(myX, myY, zoom, width, height):
-	maxiter = zoom*50 # number of iterations before escape
-	xFocus = (myX + (1.75/zoom))
-	yFocus = -(myY + (-1/zoom))
-	pixels = []
-	for i in range(0, height):
-		for n in range(0, width):
-			#
-			cX = mapRange(n, 0, width, -3.5/float(zoom), 0) + xFocus
-			cY = mapRange(i, 0, height, -2/float(zoom), 0) + yFocus
-			x = 0
-			y = 0
-			iteration = 0
-			while x*x + y*y < 2*2 and iteration < maxiter:
-				# while inside bailout radius of 2 and number of iterations has not reached the maximum
-				xtemp = x*x - y*y + cX
-				y = 2*x*y + cY
-				x = xtemp
-				iteration += 1
-			if iteration == maxiter:
-				pixels.append((0,0,0))
-			else:
-				mu = float(iteration) - log(log(sqrt(x*x+y*y))) / log(2)+1 #normalized iteration count
-				iCpct = float(mu/maxiter)
-				iC = iCpct*254
-				c1 = int(iC)
-				c2 = c1 + 1
-				t = int(mapRange(linterp(c1,c2,(float)(mu % 1)),0,256,1,255))
-				pixels.append(colorMap(t))
-	return imgFromTuples('RGB',(width,height),pixels)
+    """
+    Generates a Mandelbrot set image based on the specified parameters.
+
+    Args:
+        myX (float): X coordinate of the center of the plot.
+        myY (float): Y coordinate of the center of the plot.
+        zoom (int): Zoom level of the plot.
+        width (int): Width of the plot.
+        height (int): Height of the plot.
+
+    Returns:
+        PIL.Image.Image: Image representing the Mandelbrot set.
+
+    """
+    maxiter = zoom * 50  # number of iterations before escape
+    xFocus = (myX + (1.75 / zoom))
+    yFocus = -(myY + (-1 / zoom))
+    pixels = []
+    for i in range(height):
+        for n in range(width):
+            cX = mapRange(n, 0, width, -3.5 / zoom, 0) + xFocus
+            cY = mapRange(i, 0, height, -2 / zoom, 0) + yFocus
+            x = 0
+            y = 0
+            iteration = 0
+            while x * x + y * y < 2 * 2 and iteration < maxiter:
+                xtemp = x * x - y * y + cX
+                y = 2 * x * y + cY
+                x = xtemp
+                iteration += 1
+            if iteration == maxiter:
+                pixels.append((0, 0, 0))
+            else:
+                mu = float(iteration) - log(log(sqrt(x * x + y * y))) / log(2) + 1  # normalized iteration count
+                iCpct = float(mu / maxiter)
+                iC = iCpct * 254
+                c1 = int(iC)
+                c2 = c1 + 1
+                t = int(mapRange(linterp(c1, c2, int(mu % 1)), 0, 256, 1, 255))
+                pixels.append(colorMap(t))
+    return imgFromTuples('RGB', (width, height), pixels)
 
 if __name__ == '__main__':
-	ts = datetime.now()
-	parser = ArgumentParser()
-	parser.add_argument("x", type=float, help="x coordinate of the center of the plot", default = -0.75)
-	parser.add_argument("y", type=float, help="y coordinate of the center of the plot", default = 0.0)
-	parser.add_argument("zoom", type=int, help="Zoom level of the plot", default = 1)
-	parser.add_argument("w", type=int, help="Width of the plot", default = 480)
-	parser.add_argument("h", type=int, help="Height of the plot", default = 720)
-	parser.add_argument("filename", type=str, help="Specify a filename for the output")
-	args = parser.parse_args()
-	print str(args.x) + ", " + str(args.y) + ", " + str(args.zoom) + ", " + str(args.w) + ", " + str(args.h) + ", " + args.filename + ".tiff"
-	plot = mandelbrot(args.x, args.y, args.zoom, args.w, args.h)
-	plot.save(args.filename + ".tiff")
-	print "Done!"
-	print "Finished in %i milliseconds" % int((datetime.now()-ts).total_seconds() * 1000)
+    ts = datetime.now()
+    parser = ArgumentParser()
+    parser.add_argument("x", type=float, help="x coordinate of the center of the plot", default=-0.75)
+    parser.add_argument("y", type=float, help="y coordinate of the center of the plot", default=0.0)
+    parser.add_argument("zoom", type=int, help="Zoom level of the plot", default=1)
+    parser.add_argument("w", type=int, help="Width of the plot", default=480)
+    parser.add_argument("h", type=int, help="Height of the plot", default=720)
+    parser.add_argument("filename", type=str, help="Specify a filename for the output")
+    args = parser.parse_args()
+    print(str(args.x) + ", " + str(args.y) + ", " + str(args.zoom) + ", " + str(args.w) + ", " + str(
+        args.h) + ", " + args.filename + ".tiff")
+    plot = mandelbrot(args.x, args.y, args.zoom, args.w, args.h)
+    plot.save(args.filename + ".tiff")
+    print("Done!")
+    print("Finished in {} milliseconds".format(int((datetime.now() - ts).total_seconds() * 1000)))
